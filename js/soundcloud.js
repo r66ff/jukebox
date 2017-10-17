efca5cae299294125c8ffcc27ff4e91a8128c538
Antonina Serdyukova <Antonina Serdyukova>
2017-10-17 14:23:03 -0400

    added search by track id or search term, added play, pause, stop functionality

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,26 +1,106 @@
 // SoundCloud
 class SoundCloudJukebox{
   constructor(id){
+    this.id = id;
+    this.SCplaylist = [];
+  }
+  initSC(){
     SC.initialize({
-      client_id: id
+      client_id: this.id
     });
   }
-}
-
-
-var playlist = [];
-
-SC.get("/tracks", {q: 'sashasashamarie'}).then(function(response){
-  playlist = response;
-});
-
-function play(index){
-  if( !playlist[index].player ) {
-    SC.stream(`/tracks/${playlist[index].id}`).then(function(player){
-      playlist[index].player = player;
+  getTracksById(id){
+    var promise = new Promise(function(resolve, reject){
+      SC.get('/tracks/'+ id).then(function(response){
+        resolve(response);
+      });
+    });
+    return promise;
+  }
+  getTracksByTerm(term){
+    var promise = new Promise(function(resolve, reject){
+      SC.get('/tracks', {q: term}).then(function(response){
+        var tracks = [];
+        response.forEach(function(cur){
+          tracks.push(cur);
+        });
+        resolve(tracks);
+      });
+    });
+    return promise;
+  }
+  getPlaylist(){
+    return this.SCplaylist;
+  }
+  setPlaylist(arr){
+    for(var i = 0; i < arr.length; i++){
+      this.SCplaylist.push(arr[i]);
+    }
+  }
+  streamById(trackId){
+    SC.stream('/tracks/' + trackId).then(function(player){
+      console.log(player);
       player.play();
+      // handle buttons clicks
+      document.getElementById('play-sc').addEventListener('click', function(){
+        player.play();
+      });
+      document.getElementById('pause-sc').addEventListener('click', function(){
+        player.pause();
+      });
+      document.getElementById('stop-sc').addEventListener('click', function(){
+        player.pause();
+        player.seek(0);
+      });
     });
-  } else {
-    playlist[index].player.play();
+  }
+  displaySongsResult(song){
+    var songEl = document.createElement('div');
+    var link = document.createElement('a');
+    link.href = song.permalink_url;
+    var text = document.createTextNode(song.title);
+    link.appendChild(text);
+    songEl.appendChild(link);
+
+    document.getElementById('soundcloud').appendChild(songEl);
   }
 }
+
+function init(){
+  // create SC instance based on the App Id from the config file
+  var sound = new SoundCloudJukebox(scId);
+  // init the SC connection
+  sound.initSC();
+  // 336768726
+  // get tracks based on the user input as a search and stream the first song
+  document.getElementById('search').addEventListener('change', function(){
+    var userInput = this.value;
+    var tracks = sound.getTracksByTerm(userInput); // returns a promise
+    tracks.then(function(result){
+      sound.setPlaylist(result);
+      var songs = sound.getPlaylist();
+      songs.forEach(function(cur){
+        sound.displaySongsResult(cur);
+      });
+      // stream first track from the result
+      sound.streamById(songs[0].id);
+    });
+  });
+
+  // get tracks based on the user input as a track id and stream the song
+  document.getElementById('track-id').addEventListener('change', function(){
+    var userInput = this.value;
+    var songResult = sound.getTracksById(userInput); // returns a promise
+    songResult.then(function(result){
+      // display song info in the UI
+      sound.displaySongsResult(result);
+      // convert the result into an array and set the playlist
+      var arr = [result];
+      sound.setPlaylist(arr);
+      // stream the track
+      sound.streamById(userInput);
+    });
+  });
+
+}
+init();
